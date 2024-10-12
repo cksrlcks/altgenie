d0cc1b3a0cef1839a5b7359c62c9476c084c8f4b
chanki.kim <chanki.kim89@gmail.com>
2024-10-12 20:22:33 +0900

    헤더에 안쓰는 코드 삭제

--- src/componets/Header/index.tsx
+++ src/componets/Header/index.tsx
@@ -1,8 +1,5 @@
-import Link from 'next/link';
 import Image from 'next/image';
 import styles from './style.module.css';
-import Profile from '../Profile';
-import { handleReset } from '@/utils/reset';
 
 // type TempSession = {
 //   user: {
