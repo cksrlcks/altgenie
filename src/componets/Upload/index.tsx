ce54326e17292cccb20461b4781083094f3b7501
chanki.kim <chanki.kim89@gmail.com>
2024-10-12 11:11:41 +0900

    api요청하는 부분 form태그로 수정

--- src/componets/Upload/index.tsx
+++ src/componets/Upload/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useRef, useState } from 'react';
+import { ChangeEvent, FormEvent, useRef, useState } from 'react';
 import { Button, ButtonWithWrapper } from '../common/Button';
 import Section from '../common/Section';
 import styles from './style.module.css';
@@ -16,7 +16,8 @@ export default function Upload({ onSubmit }: UploadProps) {
     setFile(e.target.files[0]);
   }
 
-  function handleSubmit() {
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
     if (!file) return;
     const formData = new FormData();
     formData.append('file', file);
@@ -25,34 +26,36 @@ export default function Upload({ onSubmit }: UploadProps) {
   }
   return (
     <Section title="이미지 업로드">
-      <div className={styles['upload']}>
-        <div
-          className={`${styles['upload__container']} ${file ? styles['active'] : ''}`}
-        >
-          {!file ? (
-            <label htmlFor="file" className={styles['upload__placeholder']}>
-              <input
-                id="file"
-                type="file"
-                onChange={handleUpload}
-                ref={inputFile}
-              />
-              <div className={styles['upload__placeholder-title']}>
-                이곳에 드래그하여 이미지를 올려주시거나, 클릭해서 파일을
-                첨부해주세요
-              </div>
-              <div className={styles['upload__placeholder-desc']}>
-                최대 5MB 이하. 최대 1000 px까지만 업로드 가능합니다.
-              </div>
-            </label>
-          ) : (
-            <img src={URL.createObjectURL(file)} alt="temp" />
-          )}
+      <form onSubmit={handleSubmit}>
+        <div className={styles['upload']}>
+          <div
+            className={`${styles['upload__container']} ${file ? styles['active'] : ''}`}
+          >
+            {!file ? (
+              <label htmlFor="file" className={styles['upload__placeholder']}>
+                <input
+                  id="file"
+                  type="file"
+                  onChange={handleUpload}
+                  ref={inputFile}
+                />
+                <div className={styles['upload__placeholder-title']}>
+                  이곳에 드래그하여 이미지를 올려주시거나, 클릭해서 파일을
+                  첨부해주세요
+                </div>
+                <div className={styles['upload__placeholder-desc']}>
+                  최대 5MB 이하. 최대 1000 px까지만 업로드 가능합니다.
+                </div>
+              </label>
+            ) : (
+              <img src={URL.createObjectURL(file)} alt="temp" />
+            )}
+          </div>
         </div>
-      </div>
-      <ButtonWithWrapper>
-        <Button onClick={handleSubmit}>대체택스트 만들기</Button>
-      </ButtonWithWrapper>
+        <ButtonWithWrapper>
+          <Button type="submit">대체택스트 만들기</Button>
+        </ButtonWithWrapper>
+      </form>
     </Section>
   );
 }
